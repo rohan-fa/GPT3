ba993f2bcf0cf66367387de459dae0c1fece2abd
rohan-fa <rohanfarzana@gmail..com>
2023-07-10 20:54:30 +1000

    working with App.css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
 import './navbar.css';
 
 function Navbar() {
   return (
-    
-    <div>Navbar</div>
+    <div className='gpt3__navbar'>
+      Navbar
+    </div>
   )
   
 }
