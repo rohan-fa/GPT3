c4fce304d55cb607a13b36ab0dcb1c2ed3c0906c
rohan-fa <rohanfarzana@gmail..com>
2023-07-17 14:26:56 +1000

    modified navBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg'
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wgpt3">What is GPT?</a></p>
+  <p><a href="#possibilities">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
 //BEM --> Block Element Modifire (for the css classNames)
 function Navbar() {
   const [toggleMenu, setToggleMenu] = useState(false);
@@ -13,11 +23,7 @@ function Navbar() {
           <img src={logo} alt="logo" />
         </div>
         <div className='gpt3__navbar-links_container'>
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibilities">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
       </div>
       <div className='gpt3__navbar-sign'>
@@ -29,10 +35,21 @@ function Navbar() {
         {toggleMenu
          ? <RiCloseLine color="fff" size={27} onClick={() => setToggleMenu(false)} /> 
          : <RiMenu3Line color="fff" size={27} onClick={() => setToggleMenu(true)} /> 
-          
         }
+        {toggleMenu && (
+          <div className='gpt3__navbar-menu_container scale-up-center'>
+            <div className='gpt3__navbar-menu_container-links'>
+              <Menu />
+              <div className='gpt3__navbar-menu_container-links-sign'>
+                <p>Sign in</p>
+                <button type="Button">Sign up</button>  
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
+    
   )
   
 }
