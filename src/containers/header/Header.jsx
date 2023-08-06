f48014dbac9840e29e49108674faa63dfdc30ed5
rohan-fa <rohanfarzana@gmail..com>
2023-08-06 18:11:21 +1000

    header container

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -3,8 +3,18 @@ import './header.css';
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <div className='gpt3__header section__padding' id="home">Header
+      <div className='gpt3__header-container'>
+        <h1 className='gradient__text'>Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
+        <div className='gpt3__header-content__input'>
+          <input type="email" placeholder='Your email address' />
+          <button type='button'>Get Started</button>
+        </div>
+      </div>
+    </div>
+  
+    )
 }
 
 export default Header
