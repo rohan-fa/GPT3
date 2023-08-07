24b0180ee8125ae51bad3c32fede8698823b94dd
rohan-fa <rohanfarzana@gmail..com>
2023-08-07 20:55:40 +1000

    fixed text css\

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './features.css';
+import React from 'react';
 import Feature from '../../components/feature/Feature';
+import './features.css';
 
-const featuersData = [
+const featuresData = [
   {
     title: 'Improving end distrusts instantly',
     text: 'From they fine john he give of rich he. They age and draw mrs like. Improving end distrusts may instantly was household applauded.',
@@ -21,22 +21,18 @@ const featuersData = [
   },
 ];
 
-function Features() {
-  return (
-    //we use id so that we can scroll to it from our navigation bar
-    <div className='gpt3__features section__padding' id="features"> 
-      <div className='gpt3__features-heading'>
-        <h1 className='gradient__text'>The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
-        <p>Request Early Access to Get Started</p>
-      </div>
-      <div className='gpt3__features-container'>
-        {/* in here we want to render 4 different components. for creating loop let create features data array at the top, we can map on the array so in react open a dynamic block of code,{featuresData.map((item,index))} */}
-        {featuersData.map((iteam, index) => (
-          <Feature />
-        ))}
-      </div>
+const Features = () => (
+  <div className="gpt3__features section__padding" id="features">
+    <div className="gpt3__features-heading">
+      <h1 className="gradient__text">The Future is Now and You Just Need to Realize It. Step into Future Today. & Make it Happen.</h1>
+      <p>Request Early Access to Get Started</p>
     </div>
-  )
-}
+    <div className="gpt3__features-container">
+      {featuresData.map((item, index) => (
+        <Feature title={item.title} text={item.text} key={item.title + index} />
+      ))}
+    </div>
+  </div>
+);
 
-export default Features
+export default Features;
