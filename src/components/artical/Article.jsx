c90557aaab194eeaa9f455191b44f37630c777f3
rohan-fa <rohanfarzana@gmail..com>
2023-08-08 20:30:48 +1000

    working on Article

--- src/components/artical/Article.jsx
+++ src/components/artical/Article.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './artical.css';
 
-function Article({imgUrl}) {
+function Article({imgUrl, title, date}) {
   return (
     <div className='gpt3__blog-container_article'>
       <div className='gpt3__blog-container_article-image'>
-        <img src={imgUrl} alt="BlogImg" />
+        <img src={imgUrl} alt="Blog" />
+      </div>
+      <div className='gpt3__blog-container_article-content'> 
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Fill Article</p>
       </div>
     </div>
   )
