517807485e12a9db30635c97f96f1734317661ca
rohan-fa <rohanfarzana@gmail..com>
2023-08-08 16:58:10 +1000

    working on the blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import './blog.css';
+import Artical from '../../components/artical/Artical'
 
 function Blog() {
   return (
-    <div>Blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container-groupA'>
+          Artical
+        </div>
+        <div className='gpt3__blog-container-groupB'>
+        Artical
+        Artical
+        Artical
+        </div>
+      </div>  
+    </div>
   )
 }
 
