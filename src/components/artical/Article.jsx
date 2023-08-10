871d9683b1fcdc6ba9de43ad98ae81565db3fbdc
rohan-fa <rohanfarzana@gmail..com>
2023-08-10 11:44:19 +1000

    footer complete

--- src/components/artical/Article.jsx
+++ src/components/artical/Article.jsx
@@ -1,22 +1,19 @@
-import React from 'react'
-import './artical.css';
+import React from 'react';
+import './article.css';
 
-function Article({imgUrl, title, date}) {
-  return (
-    <div className='gpt3__blog-container_article'>
-      <div className='gpt3__blog-container_article-image'>
-        <img src={imgUrl} alt="Blog" />
-      </div>
-      <div className='gpt3__blog-container_article-content'> 
-        <div>
-          <p>{date}</p>
-          <h3>{title}</h3>
-        </div>
-        <p>Read Fill Article</p>
-      </div>
+const Article = ({ imgUrl, date, text }) => (
+  <div className="gpt3__blog-container_article">
+    <div className="gpt3__blog-container_article-image">
+      <img src={imgUrl} alt="blog_image" />
     </div>
-  )
-  
-}
+    <div className="gpt3__blog-container_article-content">
+      <div>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
+    </div>
+  </div>
+);
 
-export default Article
+export default Article;
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,7 +8,7 @@ import './header.css';
 
 function Header() {
   return (
-    <div className='gpt3__header section__padding' id="home">Header
+    <div className='gpt3__header section__padding' id="home">
       <div className='gpt3__header-content'>
         <h1 className='gradient__text'>Let&apos;s Build Something amazing with GPT-3 OpenAI</h1>
         <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
